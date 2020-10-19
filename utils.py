2b03869838b4da22f6a56241124fa5b72be28bb7
rosalvio <josevalterna@gmail.com>
2020-10-19 19:59:13 +0200

    Levenshtein completa

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def distance(str1, str2):
   d=dict()
   for i in range(len(str1)+1):
@@ -9,3 +11,26 @@ def distance(str1, str2):
      for j in range(1, len(str2)+1):
         d[i][j] = min(d[i][j-1]+1, d[i-1][j]+1, d[i-1][j-1]+(not str1[i-1] == str2[j-1]))
   return d[len(str1)][len(str2)]
+
+def matriz(term, ref):
+   """
+   Metodo dado en el boletin
+   """
+   vref = np.array(list(ref))
+   return np.vstack([vref != letter for letter in term]) + 0
+
+
+def levenshtein(term, ref):
+   """
+   Calcula la distancia de Levenshtein entre las cadenas term y ref
+   """
+   mat = matriz(term, ref)
+   res = np.zeros(shape=(len(term)+1,len(ref)+1))
+   for i in range(0,len(term)+1):
+      for j in range(0,len(ref)+1):
+            if i==0 or j==0:
+               res[i,j] = res[i,j] + i + j
+            else:
+               # El mejor camino viene siempre dado por la diagonal
+               res[i,j] = mat[i-1,j-1] + res[i-1,j-1]
+   return res[len(term),len(ref)]
