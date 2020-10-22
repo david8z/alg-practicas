e45c9c3de706eacfda87672869db5701ebdf2cc1
David Alarcón Segarra <alarconsegarradavid@gamil.com>
2020-10-22 17:02:38 +0200

    Damerau Levehnstein Restringida

--- utils.py
+++ utils.py
@@ -1,17 +1,5 @@
 import numpy as np
 
-def distance(str1, str2):
-  d=dict()
-  for i in range(len(str1)+1):
-     d[i]=dict()
-     d[i][0]=i
-  for i in range(len(str2)+1):
-     d[0][i] = i
-  for i in range(1, len(str1)+1):
-     for j in range(1, len(str2)+1):
-        d[i][j] = min(d[i][j-1]+1, d[i-1][j]+1, d[i-1][j-1]+(not str1[i-1] == str2[j-1]))
-  return d[len(str1)][len(str2)]
-
 def matriz(term, ref):
    """
    Metodo dado en el boletin
@@ -31,11 +19,39 @@ def levenshtein(term, ref):
             if i==0 or j==0:
                res[i,j] = res[i,j] + i + j
             else:
-               
                res[i,j] =min(
                   mat[i-1,j-1] + res[i-1,j-1],
                   1 + res[i-1,j],
                   1 + res[i,j-1]
                )
                
+   return res[len(term),len(ref)]
+
+def damerau_levenstein_restringida(term, ref):
+   """
+   Calcula la distancia de Damerau Levenshtein Restringida entre las cadenas term y ref
+   """
+   # Simula el infinito
+   INF = len(term) + len(ref)
+
+   mat = matriz(term, ref)
+   res = np.zeros(shape=(len(term)+1,len(ref)+1))
+
+   for i in range(0,len(term)+1):
+      for j in range(0,len(ref)+1):
+            if i==0 or j==0:
+               res[i,j] = res[i,j] + i + j
+            elif i == 1 or j == 1:
+               res[i,j] =min(
+                  mat[i-1,j-1] + res[i-1,j-1],
+                  1 + res[i-1,j],
+                  1 + res[i,j-1],
+               )
+            else:
+               res[i,j] =min(
+                  mat[i-1,j-1] + res[i-1,j-1],
+                  1 + res[i-1,j],
+                  1 + res[i,j-1],
+                  1 + res[i-2,j-2] + (mat[i-2,j-1] + mat[i-1,j-2]) * INF
+               )
    return res[len(term),len(ref)]
