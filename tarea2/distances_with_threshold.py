9cacd23381d6d57c32328df1d643ef2a6d874ebe
rosalvio <josevalterna@gmail.com>
2020-10-25 20:48:17 +0100

    Tarea 2 opcional con distancia de Levenshtein
    Creo que lo óptimo sería que cada uno lo hiciera con la suya porque cada uno lo hemos implementado un poco de nuestra propia forma, si no puedo intentar hacerlo yo

--- tarea2/distances_with_threshold.py
+++ tarea2/distances_with_threshold.py
@@ -27,3 +27,23 @@ def dp_levenshtein_backwards_threshold(term, ref, threshold):
                 if (res.diagonal() > threshold).any():
                     return None
     return res[len(term),len(ref)]
+
+
+def levenshtein_active_states(ref,term,threshold):
+    mat = matriz(term,ref)
+    current = np.zeros((len(term)+1))
+    prev = np.arange(0,len(term)+1)
+    # prev representa la columna anterior
+    for i in range(1,len(ref)+1):
+        current[0] = prev[0] + 1
+        for j in range(1,len(term) + 1):
+            cond = ref[i-1] == term[j-1]
+            current[j] = min(
+                    cond * 1 + prev[j-1],
+                    1 + prev[j],
+                    1 + current[j-1]
+                )
+            if i == j and current[j]>threshold:
+                return None
+        prev = current
+    return current[len(term)]
